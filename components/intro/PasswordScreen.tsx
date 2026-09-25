052ebaf45db1bb8f0607741e46fb893905152e07
ShallyKaushik <shallykaushik00@gmail.com>
2026-09-25 14:43:14 +0530

    Little Updates

--- components/intro/PasswordScreen.tsx
+++ components/intro/PasswordScreen.tsx
@@ -14,7 +14,7 @@ export default function PasswordScreen({ onUnlock }: PasswordScreenProps) {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (password === "aryanbhadwu") {
+    if (password === "genesis@2022") {
       onUnlock();
     } else {
       setError(true);
